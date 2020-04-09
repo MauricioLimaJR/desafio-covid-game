6334362cc37c87ef5925cf685306a61310dd080e
Maurício Lima <mauricio.lima@zeropay.io>
2020-04-09 01:11:34 -0300

    github deploy commit

---
+++ build/precache-manifest.0ec7920d7eece40e25de16a8cf5d1714.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f37e0834b130a6d6a517758ddc023b",
+    "url": "/game-19/index.html"
+  },
+  {
+    "revision": "9b6c193bdd0cc4699b61",
+    "url": "/game-19/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "0c36543dbba38f689173",
+    "url": "/game-19/static/css/main.0a203dcb.chunk.css"
+  },
+  {
+    "revision": "9b6c193bdd0cc4699b61",
+    "url": "/game-19/static/js/2.62fd04c8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/game-19/static/js/2.62fd04c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c36543dbba38f689173",
+    "url": "/game-19/static/js/main.753f9ba5.chunk.js"
+  },
+  {
+    "revision": "3191f376183315798986",
+    "url": "/game-19/static/js/runtime-main.9242e16e.js"
+  },
+  {
+    "revision": "ebd41b878bdfb2d4708413a000bbfeb3",
+    "url": "/game-19/static/media/award.ebd41b87.svg"
+  },
+  {
+    "revision": "ca8f222005aaac6f5e77f0069cd777f0",
+    "url": "/game-19/static/media/background.ca8f2220.png"
+  },
+  {
+    "revision": "b9372177bd78aaa41e292f61f7f0d71e",
+    "url": "/game-19/static/media/book.b9372177.svg"
+  },
+  {
+    "revision": "fcb188cbb9a63733f159d1eb313ba4c0",
+    "url": "/game-19/static/media/game.fcb188cb.svg"
+  },
+  {
+    "revision": "9a872911a523a0b86029ad990bb5d5fe",
+    "url": "/game-19/static/media/virus.9a872911.svg"
+  }
+]);
